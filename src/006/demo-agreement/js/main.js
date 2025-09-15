e23dda0c3441f7458c84e16f63fb279cdb9a1949
Dan Gilleland <dgilleland@nait.ca>
2025-09-15 14:04:46 -0600

    Completed Demo Agreement

--- src/006/demo-agreement/js/main.js
+++ src/006/demo-agreement/js/main.js
@@ -8,3 +8,28 @@
 
 	Use the skills you have learned up to this point.
 */
+let contractDate = document.querySelector('.date');
+contractDate.innerText = 'Sep 15, 2025';
+
+// I'll capture the names of the two parties of the contract
+// by using the prompt() function.
+let disclosingParty = prompt('Enter the name of the disclosing party:');
+let receivingParty = prompt('Enter the name of the receiving party:');
+
+
+// I'll use a general variable for the element I'm looking for
+let element;
+element = document.querySelector('.disclosing-party');
+//                                \ CSS Class Name /
+element.innerText = disclosingParty;
+
+// Take a few minutes to locate and set the value for the receiving party's name.
+element = document.querySelector('.receiving-party');
+element.innerText = receivingParty;
+
+element = document.querySelector('.signature .disclosing-party');
+element.innerText = disclosingParty;
+
+element = document.querySelector('.signature .receiving-party');
+element.innerText = receivingParty;
+
