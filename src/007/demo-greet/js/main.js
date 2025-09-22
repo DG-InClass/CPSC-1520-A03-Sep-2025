f94c580442990554e374de2d163806e2d2d52d51
Dan Gilleland <dgilleland@nait.ca>
2025-09-22 13:59:25 -0600

    Intro to functions

--- src/007/demo-greet/js/main.js
+++ src/007/demo-greet/js/main.js
@@ -1,10 +1,23 @@
 // This demonstrates using a parameter
 // e.g. greetUser('Jane Doe');
 // e.g. greetUser(prompt('Name:'));
+let originalTitle = document.querySelector('h1.intro').innerText;
 
+// This is an example of Function Declaration
 function greetUser(username) {
     // select the h1
     var mainTitle = document.querySelector('h1.intro');
     // update the h1 inner HTML
-    mainTitle.innerHTML = 'Welcome ' + username + ' to ' + mainTitle.innerHTML;
+    mainTitle.innerHTML = 'Welcome ' + username + ' to ' + originalTitle;
 };
+
+// An example of calling the function
+greetUser('Stewart Dent');
+
+// We can also create our functions using Function Expressions
+// I prefer this approach when used along with the const keyword
+const describeImage = function(description) {
+    let imgElement = document.querySelector('img');
+    imgElement.alt = description;
+    imgElement.title = description;
+}
