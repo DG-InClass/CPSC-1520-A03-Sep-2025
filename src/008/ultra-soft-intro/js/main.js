9ce87d88eb1864d173cb9122dfc94b2024253413
Dan Gilleland <dgilleland@nait.ca>
2025-09-24 13:40:00 -0600

    Demo ultra-soft intro

--- src/008/ultra-soft-intro/js/main.js
+++ src/008/ultra-soft-intro/js/main.js
@@ -1,8 +1,21 @@
 let counter = 0;
+
+// This is going to be my handler for click events
 const increment = function() {
     counter++;
     document.querySelector('div').innerText = `Clicked ${counter} times.`;
 }
 
+// Get a reference to the DOM element I want to add my listeners to
 const theButton = document.querySelector('button');
+// Wire-up or "attach" an "event listener" or "handler" for a particular event
 theButton.addEventListener('click', increment);
+//                         \_____/  \_______/
+//                          | string    |
+//                                  A function to be called when the event occurs
+//                                  A "callback"
+//                                  A "delegate"
+
+theButton.addEventListener('dblclick', function() { counter = 0; });
+//                                     \_________________________/
+//                                       | An inline function
