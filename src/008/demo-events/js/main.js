8933d94863ceab84cd90c8e92179aa09443d1de6
Dan Gilleland <dgilleland@nait.ca>
2025-09-29 13:32:56 -0600

    Started moving some code to JS module

--- src/008/demo-events/js/main.js
+++ src/008/demo-events/js/main.js
@@ -2,39 +2,48 @@
     - Show the paragraph with the poem to the user when the user clicks on the image.
 
 */
-// 1. Create a function to show the poem
-const handleFeatureClick = function() {
-    console.log('img.feature was clicked...');
-    let desc = document.querySelector('.feature.description');
-    // The next line will remove the CSS class `hidden` from the element
-    // All HTML elements will have 
-    //    .classList   <-- The CSS Class Names on the element
-    //        .add('css-class')
-    //        .remove('css-class')
-    //        .toggle('css-class')
-    desc.classList.remove('hidden');
-}
+import { showElementOnClick } from './eventListeners';
 
-// 2. Cause my function to be called when the user clicks on the image
-//    (I use the .addEventListener() on my DOM object to hook up my "response" to the "event")
+let desc = document.querySelector('.feature.description');
 let featureImage = document.querySelector('img.feature');
-featureImage.addEventListener('click', handleFeatureClick);
-//                            \_____/  \________________/
-//                             |         |- the name of the function I want to run
-//                             | the name of the DOM event that I am "listening" for
-
-// Let's use the same event handler (handlFeatureClick) for a listener on another element
 let headerEl = document.querySelector('header > h1');
-headerEl.addEventListener('click', handleFeatureClick);
 
-// 3. Let's use a double-click on the header element to HIDE the description.
-//    This time, instead of passing a function by name for the event handler,
-//    we'll pass in an anonymous function.
-headerEl.addEventListener('dblclick', function () {
-    var desc = document.querySelector('.feature.description');
-    desc.classList.add('hidden');
-})
-//    An "anonymous function" is a function without a name.
+showElementOnClick(featureImage, desc);
+showElementOnClick(headerEl, desc);
+
+// // 1. Create a function to show the poem
+// const handleFeatureClick = function() {
+//     console.log('img.feature was clicked...');
+//     let desc = document.querySelector('.feature.description');
+//     // The next line will remove the CSS class `hidden` from the element
+//     // All HTML elements will have 
+//     //    .classList   <-- The CSS Class Names on the element
+//     //        .add('css-class')
+//     //        .remove('css-class')
+//     //        .toggle('css-class')
+//     desc.classList.remove('hidden');
+// }
+
+// // 2. Cause my function to be called when the user clicks on the image
+// //    (I use the .addEventListener() on my DOM object to hook up my "response" to the "event")
+// let featureImage = document.querySelector('img.feature');
+// featureImage.addEventListener('click', handleFeatureClick);
+// //                            \_____/  \________________/
+// //                             |         |- the name of the function I want to run
+// //                             | the name of the DOM event that I am "listening" for
+
+// // Let's use the same event handler (handlFeatureClick) for a listener on another element
+// let headerEl = document.querySelector('header > h1');
+// headerEl.addEventListener('click', handleFeatureClick);
+
+// // 3. Let's use a double-click on the header element to HIDE the description.
+// //    This time, instead of passing a function by name for the event handler,
+// //    we'll pass in an anonymous function.
+// headerEl.addEventListener('dblclick', function () {
+//     var desc = document.querySelector('.feature.description');
+//     desc.classList.add('hidden');
+// })
+// //    An "anonymous function" is a function without a name.
 
 // 4. Let's listen for the mouseover event on the image and log the mouse position to the console
 featureImage.addEventListener('mouseover', function(event) {
