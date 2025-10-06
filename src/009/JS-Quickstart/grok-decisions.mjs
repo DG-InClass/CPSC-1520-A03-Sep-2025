42257f4d882d8c253485ad3cbd446100ab6cb828
Dan Gilleland <dgilleland@nait.ca>
2025-10-06 13:35:08 -0600

    Step 1a)

--- src/009/JS-Quickstart/grok-decisions.mjs
+++ src/009/JS-Quickstart/grok-decisions.mjs
@@ -7,4 +7,39 @@ let nothing, something = 42;
 let message;
 message = `"nothing" is '${ify(nothing)}' and "something" is '${ify(something)}'\n`;
 console.log(message);
+message = `"true" is ${ify(true)}\n`;
+//                         \__/  Boolean
+console.log(message);
+// Try calling describeDataType() and getDataType() to see what they return.
+console.log(describeDataType('Stewart Dent'));
+console.log(getDataType('Stewart Dent'));
+// Create an object using Object Literal syntax
+let person = {
+    name: 'Stewart',
+    age: 23
+};
+console.log(describeDataType(person));
+console.log(getDataType(person));
 
+console.clear();
+console.log('Step 1)');
+// Variables may be re-used in subsequent examples
+let success = true;
+let isEmployed = false; // Don't worry, you'll get there...
+
+/* The If-Else statement follows this grammar:
+
+if(conditionalExpression)
+    statementOrStatementBlock
+else
+    statementOrStatementBlock
+
+where the "conditionalExpression" is some expression that will result in a true or false value, and the "statementOrStatementBlock" is either a single instruction/statement or a set of statements inside curly braces (statement block).
+*/
+
+// Step 1) A Simple if Statement
+//          At the heart of our if statement is the conditional expression
+if(success == true) {
+    console.log('Success! The code inside this "block" will run.');
+    console.log('Read on for more discoveries.\n');
+}
