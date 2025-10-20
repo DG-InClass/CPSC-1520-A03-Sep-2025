6a6b8da6269a76af0de633ed893a4c42ce7a14d1
Dan Gilleland <dgilleland@nait.ca>
2025-10-20 13:58:07 -0600

    Comments on arg-parser.js
    This is both instructive about arrays and a practical example of why we need them

--- src/011/JS-Quickstart/arg-parser.js
+++ src/011/JS-Quickstart/arg-parser.js
@@ -5,10 +5,31 @@ const log = console.log;
 
 log('Parsing args like a CLI...');
 log(`There are ${process.argv.length} values passed into NodeJS:`);
+// The process.argv array is an array of
+// strings. When calling node in the 
+// terminal, you are passing in a
+// space-delimited (spaces to separate)
+// set of strings
+
+// The following for..of loop goes through each item
+// in the array. (The for..of statement is like the
+// foreach statement in C#.)
 for(let arg of process.argv) {
+    log('  - ', arg); // just listing an item
+}
+/* The line above can be thought of as the equivalent of
+   the following:
+for(let index = 0; index < process.argv.length; index++) {
+    let arg = process.argv[index];
     log('  - ', arg);
 }
 
+ */
+
+// In Node, because knowing WHERE node is installed is so
+// important, there's an additional property on the process
+// object called `argv0`, which is the same as 
+// process.argv[0]   <==  "process dot argv at position zero"
 if(process.argv[0] === process.argv0) {
     log('\nThe special .argv0 property is the path to Node on your computer.\n')
 }
@@ -18,8 +39,15 @@ if(process.argv[0] === process.argv0) {
 let selectedDemo = "";
 let onOneLine = false;
 
+// Processing the arguments for my command-line app
+// I am skipping the first two strings because those
+// were "special-purpose" values used by Node itself.
+//      --demo factorial --one-line
+// The loop below is my "discovering" what flags + values
+// are being sent into this program.
 for(let index = 2; index < process.argv.length; index++) {
     let arg = process.argv[index];
+    // Check to see what demo they want to run
     if(arg === "--demo" && index + 1 < process.argv.length) {
         selectedDemo = process.argv[index + 1];
         index++; // skip one
@@ -29,7 +57,8 @@ for(let index = 2; index < process.argv.length; index++) {
     }
 }
 
-if(selectedDemo) {
+// After parsing the input, run whatever demo they asked for
+if(selectedDemo === "factorial") {
     let factorials = [];
     for(let count = 1; count < 10; count++) {
         let value = calculateFactorial(count);
@@ -47,6 +76,7 @@ if(selectedDemo) {
     }
 }
 
+// The actual demo functions....
 function calculateFactorial(number) {
     let fact = 1;
     for(let count = 1; count <= number; count++) {
@@ -55,3 +85,4 @@ function calculateFactorial(number) {
     let result = `${number}! is ${fact}`;
     return result;
 }
+// ... and I can add others (like calcFibonnacci)
