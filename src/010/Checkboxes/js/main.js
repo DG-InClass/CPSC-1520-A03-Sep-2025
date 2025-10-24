931105d793a29c021521946b83c7b745f3bd7ca0
Dan Gilleland <dgilleland@nait.ca>
2025-10-24 11:29:35 -0600

    querySelectorAll()

--- src/010/Checkboxes/js/main.js
+++ src/010/Checkboxes/js/main.js
@@ -1,1 +1,21 @@
 // Enter JavaScript for the exercise here...
+// 1) Add an event listener for <input type="checkbox" name="toggle" />
+const toggleAll = document.querySelector('[name=toggle]');
+toggleAll.addEventListener('change', function (evt) {
+    evt.preventDefault();
+    const target = evt.target;
+    console.log(`main toggle is ${target.checked ? 'on' : 'off'}`);
+    //                            \_____ ternary expression __/
+
+    // 2) get all the checkboxes - NodeList
+    const allCheckboxes = document.querySelectorAll('[name=message]');
+    console.log(allCheckboxes);
+
+    // 3) make all those checkboxes match the .checked state of my target
+    let index = 0;
+    while(index < allCheckboxes.length) {
+        allCheckboxes[index].checked = target.checked;
+        index += 1;
+    }
+});
+
