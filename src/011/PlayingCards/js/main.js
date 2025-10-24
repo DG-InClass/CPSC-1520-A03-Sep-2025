983a25e0e223e409fda2c6729af30037939dbc5b
Dan Gilleland <dgilleland@nait.ca>
2025-10-24 11:47:54 -0600

    Demo dealing playing cards

--- src/011/PlayingCards/js/main.js
+++ src/011/PlayingCards/js/main.js
@@ -2,9 +2,42 @@ const container = document.getElementById('players');
 
 const form = document.querySelector('form');
 // TODO: Add Event Listener
+form.addEventListener('submit', function (evt) {
+    evt.preventDefault(); // ALWAYS for our submit event handlers/forms in CPSC-1520
+    console.log('Adding players...');
+
+    // Get the names
+    const inputNames = evt.target.elements.playerNames;
+    const names = inputNames.value.split(',').map(name => name.trim());
+    // What does the line above do??
+
+    if(names.length > 0) {
+        // Add players to the container
+        names.forEach((player, index) => {
+            container.innerHTML += `<div id="player-${index}"><h2>${player}</h2></div>`;
+        });
+        // console.log(container); // un-comment this if you are having problems....
+
+        // Disable button, hide form, show container
+        evt.target.elements.createPlayers.setAttribute('disabled', 'true');
+        evt.target.classList.add('hidden');
+        container.classList.remove('hidden');
+    }
+});
 
 const dealButton = document.getElementById('deal-me-in');
 // TODO: Add Event Listener
+dealButton.addEventListener('click', function(evt) {
+    console.clear();
+    const players = container.querySelectorAll('div');
+    // I can use the .forEach() function that comes with the NodeList data type
+    players.forEach(playerHand => {
+        const card = shuffledCards.pop();
+        const img = `<img src="/img/${card}.svg" alt="${card}" />`;
+        playerHand.innerHTML += img;
+        // console.log(playerHand); // uncomment if you are having problems
+    });
+});
 
 const shuffledCards = [
     "4D",
