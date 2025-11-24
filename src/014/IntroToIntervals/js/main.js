7b16a74db160087a340bc1e5531199170c068c2e
Dan Gilleland <dgilleland@nait.ca>
2025-11-24 13:47:09 -0700

    Demo the setInterval() function

--- src/014/IntroToIntervals/js/main.js
+++ src/014/IntroToIntervals/js/main.js
@@ -1,11 +1,23 @@
 const addDots = function() {
     let myOutput = document.getElementById('dots');
-    myOutput.innerText += '.';
+    myOutput.innerText += '.'; // append a dot to the text in that <output>
     myOutput.setAttribute('title', `There are ${myOutput.innerText.length} dots`);
 }
+
+// Start an interval
+let cancellationRef = setInterval(addDots, 1250); // 1.25 second delay
 
 // Add an event listener to the button that will stop the interval.
 document.querySelector('button')
         .addEventListener('click', function(evt) {
-            console.log('Button clicked');
+            console.log('Button clicked:', cancellationRef);
+            // The conditional expression is being tested for "truthyness"
+            if(cancellationRef) {
+                clearInterval(cancellationRef);
+                cancellationRef = 0; // Indicate that the interval has been cancelled
+                evt.target.innerText = "Continue Interval";
+            } else {
+                cancellationRef = setInterval(addDots, 1250);
+                evt.target.innerText = "Stop Interval";
+            }
         });
